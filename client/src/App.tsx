143f8cab090e31693903bd9314cb65b380f4f50a
jenniferclarsson <jennifer.2.larsson@gmail.com>
2023-09-13 16:47:49 +0200

    started on basic home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import HomePresenter from './components/presenters/homePresenter';
 
 function App() {
   return (
-    <div>
-      HAIP
-    </div>
+    <HomePresenter/>
   );
 }
 
---
+++ client/src/components/views/homeView.tsx
@@ -0,0 +1,9 @@
+import "../../assets/styles/home.scss";
+
+const HomeView = () => {
+    return (
+        <div className="welcome">Welcome to HAIP</div>
+    );
+};
+
+export default HomeView;
