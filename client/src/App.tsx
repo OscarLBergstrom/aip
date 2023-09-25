ff917f09d1bbb94daec4ae0f02b63f92490a5c8f
jenniferclarsson <jennifer.2.larsson@gmail.com>
2023-09-25 14:28:47 +0200

    Move handleLogin to model

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
         <Route
           path="/"
           element={
-            <HomePresenter />
+            <HomePresenter model={haipModel}/>
             }
          />
          <Route
--- client/src/models/model.ts
+++ client/src/models/model.ts
@@ -1,11 +1,14 @@
 export default class HaipModel {
 
   botResponse: string;
+  urlResponse: string;
 
   constructor() {
     this.botResponse = "";
+    this.urlResponse = "";
   }
 
+  /* Submit chatbot request and set the bot response */
   async submitBotRequest(userMessage: string) {
     try {
       const response = await fetch("http://localhost:3001/api/chatbot", {
@@ -28,5 +31,52 @@ export default class HaipModel {
     }
     console.log("ChatBot: \n", this.botResponse);
   }
+
+  /* Login */
+  handleLogin = async () => {
+    function generateCodeVerifier(length: number) {
+      let text = "";
+      let possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+    }
+
+    async function generateCodeChallenge(codeVerifier: string) {
+      const data = new TextEncoder().encode(codeVerifier);
+      const digest = await window.crypto.subtle.digest("SHA-256", data);
+      return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+    }
+
+    try {
+      const generated_verifier = generateCodeVerifier(128);
+      const challenge = await generateCodeChallenge(generated_verifier);
+
+      localStorage.setItem("verifier", generated_verifier);
+      const apiUrl = `http://localhost:3001/api/login?challenge=${encodeURIComponent(
+        challenge
+      )}`;
+      const response = await fetch(apiUrl, {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      this.urlResponse = data.urlResponse;
+      // document.location = data.urlResponse;
+    } catch (error) {
+      console.error("Error:", error);
+      this.urlResponse = "An error occurred while logging in."
+    }
+  };
 
 };
