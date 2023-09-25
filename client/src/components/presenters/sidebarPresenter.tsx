877980f8a75e9b426e0a01d275fb2ec32967f1ca
jenniferclarsson <jennifer.2.larsson@gmail.com>
2023-09-25 15:44:03 +0200

    sidebar closes on click outside

--- client/src/components/presenters/sidebarPresenter.tsx
+++ client/src/components/presenters/sidebarPresenter.tsx
@@ -1,19 +1,27 @@
 import SidebarView from "../views/sidebarView";
 import { useState } from "react";
+import { useOutsideClick } from "../../hooks/useOutsideClick";
 
 const SidebarPresenter = () => {
 
     const [showSidebar, setShowSidebar] = useState<boolean>(false);
+    const ref = useOutsideClick(() => {
+        if (showSidebar) {
+            setShowSidebar(false);
+        }
+      });
 
     const toggleShowSidebar = () => {
         setShowSidebar(!showSidebar);
     };
 
     return (
-        <SidebarView 
-            showSidebar={showSidebar}
-            toggleShowSidebar={toggleShowSidebar}
-        />
+        <div ref={ref}>
+            <SidebarView 
+                showSidebar={showSidebar}
+                toggleShowSidebar={toggleShowSidebar}
+            />
+        </div>
     );
 }
 
