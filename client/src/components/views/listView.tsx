4f527f0ab83e7103318e2351d575d7852698cc98
jenniferclarsson <jennifer.2.larsson@gmail.com>
2023-10-02 18:29:42 +0200

    my playlists page styles

--- client/src/components/views/listView.tsx
+++ client/src/components/views/listView.tsx
@@ -1,11 +1,23 @@
 import "../../assets/styles/common.scss";
 import "../../assets/styles/list.scss";
+import temp_logo from "../../assets/images/temp_logo.png";
 
 const ListView = () => {
+
+    const playlists = ["Playlist 1", "Playlist 2", "Playlist 3", "Playlist 3", "Playlist 3", "Playlist 3", "Playlist 3", "Playlist 3", "Playlist 3", "Playlist 3", "Playlist 3"];
+
     return (
         <div className="page">
             <div className="card">
                 <div className="title">My Playlists</div>
+                <div className="list">
+                    {playlists.map((playlist, index) => (
+                        <div key={index} className="list-item">
+                            <img className="list-item-image" src={temp_logo} alt="temp logo"/>
+                            <div className="list-item-name">{playlist}</div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
