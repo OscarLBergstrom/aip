d2375615a1abaeb622544c7f99a9beed517f6067
Oscar Bergstrom <osbergs@kth.se>
2023-10-09 16:53:18 +0200

    saves new users and gets the id of old users

--- server/src/controllers/userIdController.ts
+++ server/src/controllers/userIdController.ts
@@ -1,22 +1,38 @@
 import { Request, Response } from "express";
 import pool from "../../db-config"
 
-export const getUserIDResponse = async (req: Request, res: Response) => {
-    //TODO: Recieve User_ID in request
-
-    //TODO: Check if the USER_ID exists
-
-    //TODO: Add the USER_ID to the database
-    
-    //TODO: Send response with success or error
-  
-    try { 
-        pool.query(`SELECT USER_ID FROM haip.users`, (err:any, queryRes:any)=>{
-        console.log(queryRes);
-        res.json({
-          data: queryRes,
+export const checkUserID = async (req: Request, res: Response) => {
+    try{
+      //TODO: Recieve User_ID in request
+      var userid: string = req.body.userid;
+      try{
+        //Check if the USER_ID exists
+        var sql = "SELECT USER_ID FROM haip.users WHERE USER_ID = ?";
+        pool.query(sql, userid, (err:any, queryRes:any)=>{
+          console.log(queryRes);
+          
+          if(queryRes[0].USER_ID === userid){
+            return res.json({
+              userid: queryRes[0].USER_ID,
+            });
+          } 
+          
         });
-    })
+      }catch(error){
+        //TODO: Add the USER_ID to the database
+        var sql = "INSERT INTO haip.users (USER_ID) VALUES (?);"
+        pool.query(sql, userid, (err:any, queryRes:any)=>{
+          if(err === null){
+            return res.json({
+                success: queryRes,
+              });
+            }
+          else{
+            console.log(err);
+          }
+        });
+      }
+    
     } catch (error) {
       console.error("Error:", error);
       res
