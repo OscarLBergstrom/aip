12e96956879ff9e9908970e3d24fcff486cecca2
Oscar Bergstrom <osbergs@kth.se>
2023-10-09 17:42:35 +0200

    small changes

--- server/src/controllers/userIdController.ts
+++ server/src/controllers/userIdController.ts
@@ -2,38 +2,47 @@ import { Request, Response } from "express";
 import pool from "../../db-config"
 
 export const checkUserID = async (req: Request, res: Response) => {
-    try{
-      //TODO: Recieve User_ID in request
-      var userid: string = req.body.userid;
-      try{
+    
+    // Recieve User_ID in request
+
+    var userid: string = req.body.userid;
+    
+    try{      
+      
         //Check if the USER_ID exists
         var sql = "SELECT USER_ID FROM haip.users WHERE USER_ID = ?";
         pool.query(sql, userid, (err:any, queryRes:any)=>{
           console.log(queryRes);
           
-          if(queryRes[0].USER_ID === userid){
-            return res.json({
-              userid: queryRes[0].USER_ID,
-            });
-          } 
-          
-        });
-      }catch(error){
-        //TODO: Add the USER_ID to the database
-        var sql = "INSERT INTO haip.users (USER_ID) VALUES (?);"
-        pool.query(sql, userid, (err:any, queryRes:any)=>{
-          if(err === null){
-            return res.json({
-                success: queryRes,
+          try{
+            if(queryRes[0].USER_ID === userid){
+              return res.json({
+                userid: queryRes[0].USER_ID,
               });
             }
-          else{
-            console.log(err);
+          } catch(error){
+            
+            // If the user does not exists add the USER_ID to the database
+            
+            var sql = "INSERT INTO haip.users (USER_ID) VALUES (?);"
+            pool.query(sql, userid, (err:any, queryRes:any)=>{
+              
+              if(err === null){
+                return res.json({
+                    success: queryRes,
+                  });
+                }
+
+              else{
+                res.status(500).json({error: err}) //Handle error response
+              }
+
+            });
+
           }
+
         });
-      }
-    
-    } catch (error) {
+      } catch (error) {
       console.error("Error:", error);
       res
         .status(500)
